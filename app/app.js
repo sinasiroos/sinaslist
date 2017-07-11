4d09d3799d630483053ab7c69dd9a7df7de423a1
WIDEORBIT\ssyrous <ssyrous@wideorbit.com>
2017-07-11 22:07:06 +0200

    Initial push to github

---
+++ app/app.js
@@ -0,0 +1,57 @@
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
+
+myNinjaApp.config(['$routeProvider', function($routeProvider){
+
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'views/home.html',
+      controller: 'NinjaController'
+    }).
+    when('/directory', {
+      templateUrl: 'views/directory.html',
+      controller: 'NinjaController'
+    }).otherwise({
+      redirectTo: '/home'
+    });
+
+}]);
+
+myNinjaApp.directive('randomNinja', [function(){
+
+  return {
+    restrict: 'E',
+    scope: {
+      ninjas: '=',
+      title: '='
+    },
+    templateUrl: 'views/randomninja.html',
+    controller: function($scope){
+      $scope.random = Math.floor(Math.random() * 4);
+    }
+  };
+
+}]);
+
+myNinjaApp.controller('NinjaController', ['$scope', '$http', function($scope, $http){
+
+$scope.removeNinja = function(ninja){
+  var removedNinja = $scope.ninjas.indexOf(ninja);
+  $scope.ninjas.splice(removedNinja, 1);
+};
+
+$scope.addNinja = function(){
+  $scope.ninjas.push({
+    name: $scope.newninja.name,
+    belt: $scope.newninja.belt,
+    rate: parseInt($scope.newninja.rate),
+    available: true
+  });
+  $scope.newninja.name = "";
+  $scope.newninja.belt = "";
+  $scope.newninja.rate = "";
+};
+
+$http.get('data/ninjas.json').success(function(data){
+    $scope.ninjas = data;
+  });
+}]);
